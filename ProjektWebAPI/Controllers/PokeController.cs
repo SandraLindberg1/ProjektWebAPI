9f77f964fb9f00698f2611bdc71238ab4d88e1c4
Sandra Lindberg <70195807+SandraLindberg1@users.noreply.github.com>
2022-03-01 10:39:01 +0100

    Add project files.

---
+++ ProjektWebAPI/Controllers/PokeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProjektWebAPI.Models;
+using ProjektWebAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjektWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokeController : ControllerBase
+    {
+
+        private readonly PokeDbContext _context;
+
+        public PokeController(PokeDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+
+        public async Task<ActionResult<IEnumerable<PokeDTO>>> Get()
+        {
+            return await _context.Pokeapis.Select(t => t.ToDTO()).ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PokeDTO>> GetPoke(int id)
+        {
+            var poke = await _context.Pokeapis.FindAsync(id);
+
+            if (poke == null)
+            {
+                return NotFound();
+            }
+
+            return poke.ToDTO();       
+        
+        }
+
+
+
+        [HttpPost]
+        public async Task<ActionResult<PokeDTO>> PostTodo(PokeDTO dto)
+        {
+            var poke = dto.ToModel();
+            _context.Pokeapis.Add(poke);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPokeapi", new { id = poke.Id }, dto);
+        }
+
+    }
+}
---
+++ ProjektWebAPI/Models/Pokeapi.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ProjektWebAPI.Models
+{
+    public class Pokeapi : PokeDTO
+    {
+        public int Id { get; set; }       
+    }
+
+    public class PokeDTO
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+
+        public Pokeapi ToModel()
+        {
+            return new Pokeapi
+            {
+                Name = this.Name,
+                Url = this.Url,
+            };
+        }
+        public PokeDTO ToDTO()
+        {
+            return this;
+        }
+
+
+
+    }
+}
